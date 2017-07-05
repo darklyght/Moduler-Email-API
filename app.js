b28e1ab49aa2f964b7802083cf5f64638795f606
Kat Li Yang <kat.liyang@gmail.com>
2017-07-05 15:59:19 +0800

    Add basic authentication

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+var express = require('express');
+var body_parser = require('body-parser');
+var basic_auth = require('express-basic-auth')
+const smtp_data = require('../smtp_api.json')
 var app = express();
- 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.options("/*", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Methods', 'POST,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.sendStatus(200);
+});
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({ extended: true }));
+app.use(basic_auth({
+  "users": smtp_data.users
+}));
  
 var routes = require("./routes/routes.js")(app);
  
 var server = app.listen(3000, function () {
-    console.log("Listening on port %s...", server.address().port);
+  console.log("Listening on port %s...", server.address().port);
 });
